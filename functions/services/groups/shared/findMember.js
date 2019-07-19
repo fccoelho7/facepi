ff7adaa5261ef05b164d5d4960169e077985ecd5
Fabio Coelho <fccoelho7@gmail.com>
2019-07-18 22:33:38 -0300

    Perfomance improvements

--- functions/services/groups/shared/findMember.js
+++ functions/services/groups/shared/findMember.js
@@ -5,11 +5,11 @@ const findMember = async (page, member) => {
   await page.click(fieldSelector, { clickCount: 4 });
   await page.keyboard.press("Backspace");
 
-  await page.waitFor(2000);
+  await page.waitFor(1000);
 
   await page.type(fieldSelector, member.name);
 
-  await page.waitFor(3000);
+  await page.waitFor(2000);
 
   const foundMember = await page.$(`#search_${member.id}`);
 
